a9a0048ca0743be40a7d894e4dc65eb534dfb11c
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2025-04-09 18:00:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+  },
 })
+
+
